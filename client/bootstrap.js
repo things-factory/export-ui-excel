b7096917115140752a26d59601368547734fb14b
leemyunggyu <jaylee@hatiolab.com>
2019-06-13 14:51:16 +0800

    v0.0.1

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,17 +1,37 @@
 import { UPDATE_EXTENSION } from '@things-factory/export-base'
 import { store } from '@things-factory/shell'
+import * as XLSX from 'xlsx'
 
-function jsonToExcel() {}
+function jsonToXslx(params) {
+  jsonToExcel('xlsx', params)
+}
+
+function jsonToXls(params) {
+  jsonToExcel('xls', params)
+}
+
+function jsonToExcel(exts, params) {
+  const sheetName = params.sheetName
+  const header = params.header
+  const data = params.data
+
+  const wb = XLSX.utils.book_new()
+  const ws = XLSX.utils.json_to_sheet(data, { header })
+  XLSX.utils.book_append_sheet(wb, ws, sheetName)
+  XLSX.writeFile(wb, `${sheetName}.${exts}`, {
+    bookType: exts
+  })
+}
 
 export default function bootstrap() {
   store.dispatch({
     type: UPDATE_EXTENSION,
     extensions: {
       xlsx: {
-        export: jsonToExcel
+        export: jsonToXslx
       },
       xls: {
-        export: jsonToExcel
+        export: jsonToXls
       }
     }
   })
